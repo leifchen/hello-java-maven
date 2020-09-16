f86b4a6d2b6dde3ba1c0b43a9b7f0e5dbe4cc1bc
LeifChen <leifchen90@gmail.com>
2020-09-16 10:13:11 +0800

    [hello-concurrency]feat: use wait & notify implement producer & consumer design pattern

---
+++ hello-concurrency/src/main/java/com/chen/concurrency/thread/ProducerConsumerModel.java
@@ -0,0 +1,86 @@
+package com.chen.concurrency.thread;
+
+import java.util.Date;
+import java.util.LinkedList;
+
+/**
+ * 生产者-消费者设计模式：使用 wait & notify 实现
+ * <p>
+ * @Author LeifChen
+ * @Date 2020-09-16
+ */
+public class ProducerConsumerModel {
+
+    public static void main(String[] args) {
+        EventStorage eventStorage = new EventStorage();
+        Producer producer = new Producer(eventStorage);
+        Consumer consumer = new Consumer(eventStorage);
+        new Thread(producer).start();
+        new Thread(consumer).start();
+    }
+}
+
+class Producer implements Runnable {
+    private EventStorage storage;
+
+    public Producer(EventStorage storage) {
+        this.storage = storage;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            storage.put();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    private EventStorage storage;
+
+    public Consumer(EventStorage storage) {
+        this.storage = storage;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            storage.get();
+        }
+    }
+}
+
+class EventStorage {
+    private int maxSize;
+    private LinkedList<Date> storage;
+
+    public EventStorage() {
+        maxSize = 10;
+        storage = new LinkedList<>();
+    }
+
+    public synchronized void put() {
+        while (storage.size() == maxSize) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        storage.add(new Date());
+        System.out.println("仓库里有" + storage.size() + "个产品");
+        notify();
+    }
+
+    public synchronized void get() {
+        while (storage.size() == 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("拿到了" + storage.poll() + "，现在仓库还剩下" + storage.size());
+        notify();
+    }
+}
